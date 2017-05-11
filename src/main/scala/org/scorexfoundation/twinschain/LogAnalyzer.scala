4ad697374564e6c126cdc27ea6dd1be00693f0b8
catena <catena@protonmail.com>
2017-05-11 19:48:10 +0300

    06 data

--- src/main/scala/org/scorexfoundation/twinschain/LogAnalyzer.scala
+++ src/main/scala/org/scorexfoundation/twinschain/LogAnalyzer.scala
@@ -10,32 +10,37 @@ import scala.util.{Failure, Success, Try}
 object LogAnalyzer extends App with Calculator with Settings {
 
 
+  val RootPath = "data/newLogs/06"
   val initialTime = 1494504700000L
-  val statsLines: Seq[Iterator[(Long, Array[String])]] = Nodes.map(n => s"data/newLogs/$n.stats").map { fn =>
-    val lines: Iterator[String] = Source.fromFile(fn).getLines()
-    lines.map(_.split(":")).map(l => (BigInt(l.head).toLong, l.last.split(",")))
-  }
-  val logger = new FileLogger("data/R06_2.stats")
 
-//  logDownloader("/home/ubuntu/data/data/tails.data")
-  timeLoop(initialTime)
+  logDownloader("/home/ubuntu/data/data/tails.data", RootPath)
+  calculateStats()
 
-  def timeLoop(time: Long): Unit = {
-    Try {
-      val tails: Seq[Seq[String]] = statsLines.map(_.find(_._1 >= time).get).map(_._2.toSeq)
-      logger.appendString(time.toString + "," + analyzeTails(tails))
-    } match {
-      case Success(_) =>
-        timeLoop(time + 10000)
-      case Failure(e) =>
-        e.printStackTrace()
+  def calculateStats(): Unit = {
+    val statsLines: Seq[Iterator[(Long, Array[String])]] = Nodes.map(n => s"$RootPath/$n.stats").map { fn =>
+      val lines: Iterator[String] = Source.fromFile(fn).getLines()
+      lines.map(_.split(":")).map(l => (BigInt(l.head).toLong, l.last.split(",")))
     }
+    val logger = new FileLogger("data/R06_2.stats")
+
+    def timeLoop(time: Long): Unit = {
+      Try {
+        val tails: Seq[Seq[String]] = statsLines.map(_.find(_._1 >= time).get).map(_._2.toSeq)
+        logger.appendString(time.toString + "," + analyzeTails(tails))
+      } match {
+        case Success(_) =>
+          timeLoop(time + 10000)
+        case Failure(e) =>
+          e.printStackTrace()
+      }
+    }
+    timeLoop(initialTime)
   }
 
 
-  def logDownloader(logPath: String): Unit = {
+  def logDownloader(logPath: String, downloadPath: String): Unit = {
     Nodes.foreach { n =>
-      val command = s"scp ubuntu@$n:$logPath data/newLogs/$n.stats"
+      val command = s"scp ubuntu@$n:$logPath $downloadPath/$n.stats"
       println(command)
       command.!
     }
