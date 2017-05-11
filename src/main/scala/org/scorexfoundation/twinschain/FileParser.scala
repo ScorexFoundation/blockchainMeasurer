8ac46bdb0c07fb74572723cd5bab38ae012ac0d9
catena <catena@protonmail.com>
2017-05-11 15:29:34 +0300

    new format parser

--- src/main/scala/org/scorexfoundation/twinschain/FileParser.scala
+++ src/main/scala/org/scorexfoundation/twinschain/FileParser.scala
@@ -1,8 +1,26 @@
 package org.scorexfoundation.twinschain
 
+import scala.collection.mutable.ArrayBuffer
 import scala.io.Source
 
 object FileParser extends App with Calculator {
+
+  val nameS = "/home/pozharko/Code/StatsCollector/data/R0<PARAM>.stats"
+
+  Seq(2, 4, 6, 8).foreach{ i =>
+    newFormatParser(nameS.replace("<PARAM>", i.toString))
+  }
+
+  def newFormatParser(filename: String): Unit = {
+    var per90: ArrayBuffer[Int] = ArrayBuffer[Int]()
+    var per50: ArrayBuffer[Int] = ArrayBuffer[Int]()
+    for (line <- Source.fromFile(filename).getLines()) {
+      val values: Seq[Int] = line.replace(",|,", ",").split(",").map(_.toInt)
+      per90 += values.last
+      per50 += values(values.length - 2)
+    }
+    println(persentile(0.5, per50) + "," + persentile(0.9, per90))
+  }
 
   def oldFormatParser(filename: String): Unit = {
     for (line <- Source.fromFile(filename).getLines()) {
